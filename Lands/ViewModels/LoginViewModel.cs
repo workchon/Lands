be5db55f45d733c324e1ec65a00f65890459010f
Synergy Software <synergy@iMac-de-Synergy.local>
2020-01-20 08:58:09 -0700

    Actualizacion Binding

---
+++ Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Lands.ViewModels
+{
+    using System.Windows.Input;
+
+    public class LoginViewModel
+    {
+        #region Properties
+        public string Email { get; set; }
+
+        public string PassWord { get; set; }
+
+        public bool IsRunning { get; set; }
+
+        public bool IsRemembered { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand LoginCommand { get; set; }
+        #endregion
+    }
+}
--- Lands/ViewModels/MainViewModel.cs
+++ Lands/ViewModels/MainViewModel.cs
@@ -2,6 +2,15 @@
 {
     public class MainViewModel
     {
-        
+        #region ViewModels
+        public LoginViewModel Login { get; set; }
+        #endregion
+
+        #region Constructor
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
     }
 }
