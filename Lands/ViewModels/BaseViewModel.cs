176b023c17a5f88e8dfd314753af505fb2a7b1ac
Synergy Software <synergy@iMac-de-Synergy.local>
2020-01-20 10:35:33 -0700

    version

--- Lands/ViewModels/BaseViewModel.cs
+++ Lands/ViewModels/BaseViewModel.cs
@@ -1,10 +1,26 @@
-﻿using System;
-namespace Lands.ViewModels
+﻿namespace Lands.ViewModels
 {
-    public class BaseViewModel
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    public class BaseViewModel : INotifyPropertyChanged
     {
-        public BaseViewModel()
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetValue<T>(ref T backingField, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingField, value))
+            {
+                return;
+            }
+
+            backingField = value;
+            OnPropertyChanged(propertyName);
         }
     }
 }
--- Lands/ViewModels/LoginViewModel.cs
+++ Lands/ViewModels/LoginViewModel.cs
@@ -1,23 +1,99 @@
 ﻿namespace Lands.ViewModels
 {
     using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+    using Xamarin.Forms;
 
-    public class LoginViewModel
+    public class LoginViewModel : BaseViewModel
     {
+
+        #region Attributes
+
+        private string password;
+
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
+
         #region Properties
         public string Email { get; set; }
 
-        public string PassWord { get; set; }
+        public string Password
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
 
-        public bool IsRunning { get; set; }
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
 
         public bool IsRemembered { get; set; }
 
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
+
+        #endregion
+
+        #region Constructor
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+            this.isEnabled = true;
+        }
         #endregion
 
         #region Commands
 
-        public ICommand LoginCommand { get; set; }
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "You Must Enter an Email", "Accept");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "You Must Enter an Password", "Accept");
+                return;
+            }
+            
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+
+            if(this.Email != "p@hotmail.com" || this.password != "1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert("Error", "Email or Password incorrect", "Accept");
+                this.password = string.Empty;
+                return;
+            }
+
+            await Application.Current.MainPage.DisplayAlert(
+                "OK",
+                "Fuck yeahhh",
+                "Accept"
+            );
+
+        }
         #endregion
     }
 }
